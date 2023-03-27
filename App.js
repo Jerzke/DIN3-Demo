dfaf7e4730f4479e1a3ed71d4ddf98d58156552f
Johanna Portimo <71264870+jayportimo@users.noreply.github.com>
2023-03-27 09:17:05 +0300

    design change

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createDrawerNavigator, DrawerContentScrollView, DrawerItem, } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+import { Text } from 'react-native';
 
 import HomeScreen from './components/Home';
 import SettingScreen from './components/Settings';
@@ -7,18 +9,43 @@ import CalendarScreen from './components/Calendar';
 import HistoryScreen from './components/History';
 
 const Drawer = createDrawerNavigator();
- 
+
+function CustomDrawerContent(props) {
+  const { navigation } = props;
+  return (
+    <DrawerContentScrollView {...props}>
+      <CustomDrawerItem label="Home" to="Home" navigation={navigation} />
+      <CustomDrawerItem label="Setting" to="Setting" navigation={navigation} />
+      <CustomDrawerItem label="Calendar" to="Calendar" navigation={navigation} />
+      <CustomDrawerItem label="History" to="History" navigation={navigation} />
+    </DrawerContentScrollView>
+  );
+}
+
+function CustomDrawerItem({ label, to, navigation }) {
+  const onPress = () => {
+    navigation.navigate(to);
+  };
+  return (
+    <DrawerItem
+      label={() => <Text style={{ color: '#9A0414' }}>{label}</Text>}
+      onPress={onPress}
+    />
+  );
+}
+
 
 export default function App() {
   return (
     <NavigationContainer>
       <Drawer.Navigator 
+      drawerContent={(props) => <CustomDrawerContent {...props} />}
       screenOptions={{
         drawerStyle: {
-          backgroundColor: '#C8A2C9',
+          backgroundColor: '#111111',
         },
         headerStyle: {
-          backgroundColor: '#C8A2C9',
+          backgroundColor: '#9A0414',
         },
       }}
       initialRouteName='Home'>
