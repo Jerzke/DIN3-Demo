f510704e12d418d2bed5dfba84c6b74ef9305e52
Johanna Portimo <71264870+jayportimo@users.noreply.github.com>
2023-03-29 11:59:13 +0300

    added a background box for text in history

--- components/History.tsx
+++ components/History.tsx
@@ -8,7 +8,10 @@ export default class HistoryContainer extends Component<Props, State> {
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', backgroundColor: 'black', padding: 50, }}>
-        <Text style={{ color: 'white', fontSize: 25, fontWeight: 'bold', }}>Eventually exercise history will be here</Text>
+        <Text style={
+          { color: 'white', fontSize: 15, fontWeight: 'bold', backgroundColor: '#383838', 
+          borderWidth: 1, borderRadius: 8, padding: 10, width: 300, height: 450, }
+          }>Eventually exercise history will be here</Text>
       </View>
     );
   }
