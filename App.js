f7824400056d42febde3a92c839682be03e41aa5
Stradioto <pedrostradioto@gmail.com>
2023-04-03 11:45:27 +0200

    testing new calendar

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ import CalendarScreen from './components/Calendar';
 import HistoryScreen from './components/History';
 import TestScreen from './components/Test';
 
+//import './components/CalendarCSS.css';
 
 const Drawer = createDrawerNavigator();
 
--- components/Calendar.tsx
+++ components/Calendar.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { View, Text } from 'react-native';
 import Calendar from 'react-calendar';
 //import './CalendarCSS.css';
-//import 'react-calendar/dist/Calendar.css';
+//import './node_modules/react-calendar/dist/Calendar.css';
 
 const CalendarScreen: React.FC = () => {
 
@@ -11,11 +11,11 @@ const CalendarScreen: React.FC = () => {
   const onChange = date => {
     setDate(date);
   }
-
+  //<Calendar  onChange={onChange} value={date}/*This will always set the current day by default*//>
   return (
     <View style = {{flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'black'}}>
         <Text style={{color: 'white', fontSize:30, fontWeight: 'bold'}}>Pick a date to see trainning data:</Text>
-        <Calendar  onChange={onChange} value={date}/*This will always set the current day by default*//>
+        
     </View>
   );
 };
