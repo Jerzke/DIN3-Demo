ba58476976ad930a2e9817e8e5a9393ed73043a7
Stradioto <pedrostradioto@gmail.com>
2023-04-03 13:20:16 +0200

    new calendar working android/desktop

--- components/Calendar.tsx
+++ components/Calendar.tsx
@@ -1,22 +1,28 @@
 import React, { useState } from 'react';
 import { View, Text } from 'react-native';
-import Calendar from 'react-calendar';
-import './CalendarCSS.css';
-//import 'react-calendar/dist/Calendar.css';
+//import Calendar from 'react-calendar';
+//import './CalendarCSS.css';
+//import './node_modules/react-calendar/dist/Calendar.css';
+import NoteTaker from './notes';
+import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
 
 const CalendarScreen: React.FC = () => {
 
-  const [date, setDate] = useState(new Date());
-
-  const onChange = date => {
-    setDate(date);
-  }
-
+  //const [date, setDate] = useState(new Date());
+  
+  //const onChange = date => {
+    //setDate(date);
+  //}
+  //<Calendar  onChange={onChange} value={date}/*This will always set the current day by default*//>
   return (
     <View style = {{flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'black'}}>
         <Text style={{color: 'white', fontSize:30, fontWeight: 'bold'}}>Pick a date to see trainning data:</Text>
-        <Calendar  onChange={onChange} value={date}/*This will always set the current day by default*//>
-        {console.log(date)}
+        <Calendar 
+        onDayPress={day => {
+          console.log('selected day', day);
+        }}
+        />
+        <NoteTaker />
     </View>
   );
 };
