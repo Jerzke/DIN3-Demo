59ebaea0f03991fcba6ab0e7d54c48485173789c
Ayrton Ramirez <ayrtonramirez2018@gmail.com>
2023-04-03 09:18:41 +0300

    Making it usable

--- components/Calendar.tsx
+++ components/Calendar.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { View, Text } from 'react-native';
 import Calendar from 'react-calendar';
-import './CalendarCSS.css';
+//import './CalendarCSS.css';
 //import 'react-calendar/dist/Calendar.css';
 
 const CalendarScreen: React.FC = () => {
@@ -16,7 +16,6 @@ const CalendarScreen: React.FC = () => {
     <View style = {{flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'black'}}>
         <Text style={{color: 'white', fontSize:30, fontWeight: 'bold'}}>Pick a date to see trainning data:</Text>
         <Calendar  onChange={onChange} value={date}/*This will always set the current day by default*//>
-        {console.log(date)}
     </View>
   );
 };
