1e9f6f2aa91771467564311dbf692559473685ee
valtteeri <hietalavaltteri@gmail.com>
2023-04-19 10:45:09 +0300

    Adding webpack
    Works on web, some warnings :)

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const createExpoWebpackConfigAsync = require('@expo/webpack-config');
+
+module.exports = async function (env, argv) {
+  const config = await createExpoWebpackConfigAsync(
+    {
+      ...env,
+      babel: {
+        dangerouslyAddModulePathsToTranspile: [
+          // Ensure that all packages starting with @evanbacon are transpiled.
+          'node_modules/victory-native',
+        ],
+      },
+    },
+    argv
+  );
+  return config;
+};
